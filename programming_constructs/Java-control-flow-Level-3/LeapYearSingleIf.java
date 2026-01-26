248763f47378865b8edbab2b135132b059f1ec96
itz-ayushimishra <itzayushimishra@gmail.com>
2026-01-26 18:33:03 +0530

    [Ayushi]Added Level 3 programs

---
+++ programming_constructs/Java-control-flow-Level-3/LeapYearSingleIf.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class LeapYearSingleIf
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter an year");
+int year = sc.nextInt();
+if(year>1582)
+{
+if(year%4==0&&year%100!=0 || year%400==0)
+{
+System.out.println("Year is a Leap Year");
+}
+else
+{
+System.out.println("Year is NOT a Leap Year");
+}
+}
+sc.close();
+}
+}
+
